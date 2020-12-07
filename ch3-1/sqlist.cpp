c014f8ed691e5c3c8c235934a32d3f766bfe2cf0
KetchupLover <yyzztao@outlook.com>
2020-12-07 20:42:47 +0800

    add codes for CH3 Q1

---
+++ ch3-1/sqlist.cpp
@@ -0,0 +1,39 @@
+﻿
+#include"SqList.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	SqList<int> l1(10);
+	int e = 0;
+	int b[6] = { 1,2,2,2,3,2 };
+	SqList<int> l2(b, 6, 10);
+	l1.InsertElem(1);
+	l1.InsertElem(2);
+	l1.InsertElem(3);
+	l1.InsertElem(3);
+	l1.InsertElem(0);
+	l1.InsertElem(4);
+	l1.InsertElem(5);
+	l1.InsertElem(3);
+	l1.Show();
+	l1.Deletemin(e);//	1/(1)
+	l1.Show();
+	e = 3;
+	l1.DeleteElemE(e);//	1/(2)
+	l1.Show();
+	l2.Show();
+	l2.DeleteElemS();
+	l2.Show();
+	//cout << l1.GetLength() << endl;
+	//l1.GetElem(2,e);
+	//cout << l1.IsEmpty() << endl;
+	//e = 'c';
+	//l1.LocateElem(e);
+	//e = 'f';
+	//l1.SetElem(3, e);
+
+
+}
+
+
