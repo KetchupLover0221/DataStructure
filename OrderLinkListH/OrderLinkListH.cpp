7e6591912151fc17a8bc4b1ca33e9cf8fd609291
KetchupLover <yyzztao@outlook.com>
2020-12-09 08:54:48 +0800

    add codes for ordered LinkList with head note

---
+++ OrderLinkListH/OrderLinkListH.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include"Orderll.h"
+int main()
+{
+    int a[5] = { 1,4,3,2,5 };
+    LinkList<int> l1(a, 5);
+    l1.Show();
+    l1.InsertElemE(3);
+    l1.Show();
+    l1.DeleteElemE(3);
+    l1.Show();
+}
+
