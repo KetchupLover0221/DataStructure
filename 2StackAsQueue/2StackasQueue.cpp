eccefea1c2dd3f9f4120790b5e28cd4ef7d970e3
KetchupLover <yyzztao@outlook.com>
2020-12-29 22:59:04 +0800

    add codes for use 2 stacks as Queue

---
+++ 2StackAsQueue/2StackasQueue.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include"FakeQueue.h"
+using namespace std;
+int main()
+{
+	AsQueue AQ;
+	int a = 2;
+	int b = 3;
+	int c = 0;
+	cout << AQ.IsEmpty() << endl;
+	AQ.EnQueue(a);
+	AQ.EnQueue(b);
+	AQ.DelQueue(c);
+	cout << c << endl;
+	cout << AQ.IsEmpty() << endl;
+	return 0;
+}
+
+
+
